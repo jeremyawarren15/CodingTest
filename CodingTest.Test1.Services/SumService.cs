1b451f5e49eb9a2a16f08a306030478328eacc14
Jeremy Warren <jeremyawarren15@gmail.com>
2020-07-10 00:17:20 -0400

    Possible solution to test 1

--- CodingTest.Test1.Services/SumService.cs
+++ CodingTest.Test1.Services/SumService.cs
@@ -10,6 +10,28 @@ namespace CodingTest.Test1.Services
         {
             return $"The sum of {firstNumber} and {secondNumber} is {firstNumber + secondNumber}";
         }
+        
+        public string SumNumbers(List<int> numbers)
+        {
+            if (!numbers.Any())
+            {
+                return $"Your sum is 0";
+            }
+            if (numbers.Count == 1)
+            {
+                return $"Your number is {numbers.First()}";
+            }
+            if (numbers.Count == 2)
+            {
+                var firstNumber = numbers[0];
+                var secondNumber = numbers[1];
+                return SumNumbers(firstNumber, secondNumber);
+            }
 
+            var allButOne = numbers.GetRange(0, numbers.Count() - 1);
+            var ns = string.Join(", ", allButOne);
+
+            return $"The sum of {ns} and {numbers.Last()} is {numbers.Sum()}";
+        }
     }
 }
