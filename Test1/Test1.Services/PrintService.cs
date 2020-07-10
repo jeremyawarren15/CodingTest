c565f00ec348c2e8a5720751eb4a8101d784d9a7
Jeremy Warren <jeremyawarren15@gmail.com>
2020-07-09 23:18:39 -0400

    Added base code for Test 1

---
+++ Test1/Test1.Services/PrintService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test1.Services
+{
+    public class PrintService
+    {
+        public string PrintSumNumbers(int firstNumber, int secondNumber)
+        {
+            return $"The sum of {firstNumber} and {secondNumber} is {firstNumber + secondNumber}";
+        }
+    }
+}
---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Test1.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var printService = new PrintService();
+            Console.WriteLine(printService.PrintSumNumbers(3, 4));
+            Console.ReadKey();
+        }
+
+    }
+}
